6314114e6120f3f4f98450bdc7b82a708e60d59a
Ciro Marano <ciro@tbb.agency>
2022-06-06 21:15:13 +0200

    Tema componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Articulo from './components/Articulo';
+import Bitcoin from './components/Bitcoin';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <Articulo />
+    <Bitcoin />
+  </div> 
+    
 }
 
 export default App;
